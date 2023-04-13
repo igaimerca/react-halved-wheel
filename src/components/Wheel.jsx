19f13b37dad33dc0801246cfda931549194fb772
igaimerca <igaimerca@gmail.com>
2023-04-13 19:32:35 +0200

    feat: halved wheel

---
+++ src/components/Wheel.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Wheel = () => {
+  return (
+    <div className="wheel-container">
+      <button id="spin"></button>
+      <div class="container">
+        <div class="one">1</div>
+        <div class="two">2</div>
+        <div class="three">3</div>
+        <div class="four">4</div>
+        <div class="five">5</div>
+        <div class="six">6</div>
+        <div class="seven">7</div>
+        <div class="eight">8</div>
+      </div>
+    </div>
+  );
+};
+
+export default Wheel;
